064a1ffc2e028dcf3d67f684ca04e449755f6efe
unknown <hon_da-1998@hotmail.com>
2021-09-10 00:11:49 +0700

    Encrypted with bcryptjs

---
+++ model/user.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://localhost:27017/LoginDB';
+var bcrypt = require('bcryptjs')
+
+mongoose.connect(mongoDB, {
+    useNewUrlParser: true
+})
+
+//Connect
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Mongodb Connect Error'));
+
+// Create Schema
+var userSchema = mongoose.Schema({
+    name: {
+      type: String
+    },
+    email: {
+      type: String
+    },
+    password: {
+      type: String
+    }
+});
+
+var User = module.exports = mongoose.model('User', userSchema);
+
+module.exports.createUser = function(newUser, callback) {
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(newUser.password, salt, function(err, hash) {
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+}
+
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var router = express.Router();
-const { check, validationResult } = require("express-validator");
+var User = require('../model/user');
+
+
+const { 
+  check, 
+  validationResult 
+} = require("express-validator");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -33,6 +39,19 @@ router.post("/register",
       });
     }else{
       //Insert Data
+      var name = req.body.name;
+      var password = req.body.password;
+      var email = req.body.email;
+      var newUser = new User({
+        name:name,
+        password:password,
+        email:email
+      });
+      User.createUser(newUser, function(err, user) {
+        if (err) throw err
+      });
+      res.location('/');
+      res.redirect('/');
     }
   }
 );
